1633e5cdf550d19034caa0348808331aa43e3ef6
Kevin Norgaard <norgaark@uci.edu>
2018-08-01 00:09:53 -0700

    Added bootstrap

--- pkp-app/src/app/app.module.ts
+++ pkp-app/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import '~bootstrap/scss/bootstrap.min';
 
 import { AppComponent } from './app.component';
 
