823b6206615a6832e0dc72b83e62daaf175fe310
Kevin Norgaard <norgaark@uci.edu>
2018-08-01 19:26:13 -0700

    rough draft of nav bar

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Hey Nik,
 
 1. navigate to the directory 'pkp-app'
-2. run command 'npm install --save bootstrap', then you should see bootstrap in node_modules
+2. run command 'npm install' to install dependencies, then you should see bootstrap in node_modules
 3. run command 'ng serve -o' to see this shit live
 
 
--- pkp-app/src/app/app.module.ts
+++ pkp-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './app/header/header.component';
+import { FooterComponent } from './app/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
